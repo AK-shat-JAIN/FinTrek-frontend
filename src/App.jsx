942701d8a50a986ef0258fba82d467e9feb603a0
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2024-06-30 18:27:06 +0530

    basic redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      hy
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   )
 }
