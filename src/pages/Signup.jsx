d166d454abb5edeb2ea85a27c0506bef6ecf7368
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-23 00:54:54 +0530

    Sign-up mobile responsive done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,16 +2,20 @@ import signup from "../assets/signup.png"
 import user from "../assets/User.png"
 import email from "../assets/email.png"
 import lock from "../assets/LockKey.png"
+import signupMobile from "../assets/signup-mobile.png"
 import { Link } from "react-router-dom"
 const Signup = () => {
   return (
-    <div className="flex flex-row bg-[#2B2B2B] pb-16">
-      <div className="mr-14">
+    <div className="flex md:flex-row flex-col  bg-[#2B2B2B] pb-16">
+      <div className="mr-14 hidden md:block">
         <img src={signup} alt="" />
       </div>
-      <div className="mt-24">
-        <div className="w-[460px] text-white text-5xl font-semibold font-['Work Sans'] capitalize leading-10 mb-5">Create account</div>
-        <div className="w-[460px] text-white text-[22px] font-normal font-['Work Sans'] capitalize leading-9 mb-10">Welcome! enter your details and start creating, collecting and selling NFTs.</div>
+      <div className="md:hidden">
+        <img src={signupMobile} className="w-screen" alt="" />
+      </div>
+      <div className="md:mt-24 md:px-0 px-[9%] mt-8 md:mb-0 mb-10">
+        <div className="md:w-[460px] w-80 text-white md:text-5xl text-4xl font-semibold font-['Work Sans'] capitalize leading-10 mb-5">Create account</div>
+        <div className="md:w-[460px] w-80 text-white md:text-[22px] text-base font-normal font-['Work Sans'] capitalize md:leading-9 mb-10">Welcome! enter your details and start creating, collecting and selling NFTs.</div>
         <form action="" onSubmit={(e) => e.preventDefault()}>
           
           <div className=" flex bg-white px-5 py-4 w-80 h-11 items-center rounded-2xl">
@@ -37,7 +41,7 @@ const Signup = () => {
           <button className="w-80 h-12 bg-[#A259FF] justify-center text-white rounded-2xl text-base font-semibold">Create account</button>
         </form>
 
-        <div className="w-96 text-white text-lg font-normal font-['Work Sans'] mt-[6%]">
+        <div className="w-96 text-white text-lg font-normal font-['Work Sans'] mt-[6%] md:px-0 px-2">
           Already has an account? &nbsp;
           <Link to="/login">
             <span className="text-[#A259FF] cursor-pointer">
